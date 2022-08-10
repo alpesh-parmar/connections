d26ed6e525fda8d371a7e521eedaf0d76ba0fd61
alpesh-parmar <98450523+alpesh-parmar@users.noreply.github.com>
2022-08-10 16:38:51 +0530

    Update emailHelper.js

--- middlewares/emailHelper.js
+++ middlewares/emailHelper.js
@@ -2,19 +2,19 @@ const nodemailer = require('nodemailer');
 
 exports.sendmail=async (subject,body,emailto)=>{
 
-
+  
   const transporter = nodemailer.createTransport({
     host: process.env.EMAILHOST,//"smtp.gmail.com",  // enter host name
     port: 465, //enter port name
     secure: true, // true for 465, false for other ports    
     auth: {
       user: process.env.EMAILID,//'testascentinfo21@gmail.com', // write your smtp account user name
-      pass: 'kogsmndflkguofgi'//process.env.EMAILPASS//'Admin#123' // write your smtp account user password
+      pass: 'gjlhnycxsyvhqrrt'//process.env.EMAILPASS//'Admin#123' // write your smtp account user password
     },
     tls: {
       rejectUnauthorized: false
     },
-  });
+  });  
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
